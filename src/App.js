373157cd266c45b2f8c0d8de5ba18ca67a055b69
Lucas Panassolo <lucaspanassolo12@hotmail.com>
2021-01-14 22:43:48 -0300

    added styles

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
+import "./App.css";
+import api from "./services/api";
 import Header from "./components/Header";
 
 function App() {
-    const [ projects, setProjects ] = useState(["Desenvolvimento de app", "Front-end web"]);
+    const [projects, setProjects] = useState([]);
 
-    function handleClickAddProject() {
-        setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    useEffect(() => {
+        api.get("projects").then((response) => {
+            setProjects(response.data);
+        });
+    }, []);
+
+    async function handleClickAddProject() {
+        const response = await api.post("projects", {
+            title: `New project ${Date.now()}`,
+            owner: "Lucas",
+        });
+
+        const project = response.data;
+
+        setProjects([...projects, project]);
     }
 
     return (
         <>
             <Header title="Homepage" />
             <ul>
-                {projects.map((project, index) => {
-                    return <li key={index}>{project}</li>;
+                {projects.map((project) => {
+                    return <li key={project.id}>{project.title}</li>;
                 })}
             </ul>
             <button type="button" onClick={handleClickAddProject}>
